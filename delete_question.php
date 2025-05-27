571a0a4f18cc7ae4261d710efc9a1576fef5bdf3
Graham Albers <123661155+Galbers2@users.noreply.github.com>
2025-05-27 19:39:08 -0400

    Changed session startup

--- delete_question.php
+++ delete_question.php
@@ -1,5 +1,12 @@
 <?php
-session_start();
+session_start([
+    'cookie_lifetime' => 0, // expires on browser close
+    'cookie_httponly' => true, // JS can't access cookies
+    'cookie_secure' => isset($_SERVER['HTTPS']), // only send cookie over HTTPS
+    'use_strict_mode' => true, // reject uninitialized session IDs
+    'use_only_cookies' => true, // don't allow session ID in URL
+]);
+
 if (!isset($_SESSION['user_id'])) {
     header("Location: signup.html");
     exit;
--- login.php
+++ login.php
@@ -1,5 +1,12 @@
 <?php
-session_start();
+session_start([
+    'cookie_lifetime' => 0, // expires on browser close
+    'cookie_httponly' => true, // JS can't access cookies
+    'cookie_secure' => isset($_SERVER['HTTPS']), // only send cookie over HTTPS
+    'use_strict_mode' => true, // reject uninitialized session IDs
+    'use_only_cookies' => true, // don't allow session ID in URL
+]);
+session_regenerate_id(true);
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
